ff61a5c2a0a78a146861a5f995248f16e4767c5d
Alexander Bluhm <lamiy-develop@live.de>
2023-12-05 22:59:17 +0100

    Add day-01

---
+++ day-01/src/main.rs
@@ -0,0 +1,46 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+    let path = "./input.txt";
+    match read_lines(path) {
+        Ok(lines) => {
+            let calibration_values: Vec<String> = lines
+                .map(|line| line.unwrap_or_default().to_string())
+                .collect();
+            println!("{}", sum_calibration_values(calibration_values))
+        }
+        Err(e) => {
+            println!("There was an error reading {}: {}", path, e.to_string())
+        }
+    }
+}
+
+fn sum_calibration_values(calibration_values: Vec<String>) -> i32 {
+    let sum: i32 = calibration_values
+        .iter()
+        .map(|value| {
+            let mut numbers: Vec<char> = vec![];
+            for character in value.chars() {
+                if character.is_numeric() {
+                    numbers.push(character);
+                }
+            }
+
+            let str_number = format!("{}{}", numbers.first().unwrap(), numbers.last().unwrap());
+            return str_number.parse::<i32>().unwrap();
+        })
+        .sum();
+
+    sum
+}
+
+// https://doc.rust-lang.org/rust-by-example/std_misc/file/read_lines.html
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
